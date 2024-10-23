65d48fd2a26607f0905fe687f02b86de2324851c
李东明 <moodlee@qq.com>
2024-10-23 16:47:58 +0800

    init

--- cli/init/main.ts
+++ cli/init/main.ts
@@ -1,4 +1,4 @@
-import { args, cmdAsync, script_path } from "../.tsc/context";
+import { args, cmdAsync, copyDirectory, script_path } from "../.tsc/context";
 import { Path } from "../.tsc/System/IO/Path";
 import { File } from "../.tsc/System/IO/File";
 import { Directory } from "../.tsc/System/IO/Directory";
@@ -6,6 +6,8 @@ import { UTF8Encoding } from "../.tsc/System/Text/UTF8Encoding";
 import { Environment } from "../.tsc/System/Environment";
 import { EnvironmentVariableTarget } from "../.tsc/System/EnvironmentVariableTarget";
 import { Json } from "../.tsc/TidyHPC/LiteJson/Json";
+import { axios } from "../.tsc/Cangjie/TypeSharp/System/axios";
+import { zip } from "../.tsc/Cangjie/TypeSharp/System/zip";
 let utf8 = new UTF8Encoding(false);
 let parameters = {} as { [key: string]: string };
 for (let i = 0; i < args.length; i++) {
@@ -41,8 +43,15 @@ let repositoryDirectory = Path.Combine(OPEN_CAD_DIR, "repository");
 if (Directory.Exists(repositoryDirectory) == false) {
     Directory.CreateDirectory(repositoryDirectory);
 }
+let downloadDirectory = Path.Combine(OPEN_CAD_DIR, "download");
+let sdkDirectory = Path.Combine(OPEN_CAD_DIR, "sdk");
+if (Directory.Exists(downloadDirectory) == false) {
+    Directory.CreateDirectory(downloadDirectory);
+}
+if (Directory.Exists(sdkDirectory) == false) {
+    Directory.CreateDirectory(sdkDirectory);
+}
 
-let cadName = args[0];
 
 let GitManager = () => {
     let clone = async () => {
@@ -69,10 +78,19 @@ let GitManager = () => {
         let indexJsonPath = Path.Combine(repositoryDirectory, "index.json");
         return await Json.LoadAsync(indexJsonPath);
     };
+    let getHttpProxy = async () => {
+        let output = {} as { lines: string[] };
+        await cmdAsync(Environment.CurrentDirectory, "git config --get http.proxy", output);
+        if (output.lines && output.lines.length > 0) {
+            return output.lines[0];
+        }
+        return "";
+    };
 
     return {
         clone,
-        getIndexJson
+        getIndexJson,
+        getHttpProxy
     };
 };
 
@@ -83,10 +101,35 @@ let main = async () => {
     if (await gitManager.clone() == false) {
         return;
     }
+    let cadName = args[0];
     let indexJson = await gitManager.getIndexJson();
-    console.log(indexJson);
-    let sdks = indexJson.SDK[cadName.toUpperCase()];
-    console.log(sdks);
+    let sdks = indexJson.SDK[cadName.toUpperCase()] as {
+        name: string,
+        version: string,
+        download_url: string
+    }[];
+    if (sdks == undefined) {
+        console.log(`cadName ${cadName} not found`);
+        return;
+    }
+    // 从sdks中找到最新的版本
+    let sdk = sdks[0];
+    let proxyInfo = await gitManager.getHttpProxy();
+    if (proxyInfo != "") {
+        console.log(`http.proxy: ${proxyInfo}`);
+        axios.setProxy(proxyInfo);
+    }
+    let download_path = Path.Combine(downloadDirectory, Path.GetFileName(sdk.download_url));
+    console.log(`downloading ${sdk.download_url} to ${download_path}`);
+    await axios.download(sdk.download_url, download_path);
+    console.log(`downloaded ${download_path}`);
+    let cadDirectory = Path.Combine(sdkDirectory, cadName);
+    if (Directory.Exists(cadDirectory) == false) {
+        Directory.CreateDirectory(cadDirectory);
+    }
+    let cadSdkDirectory = Path.Combine(cadDirectory, sdk.name);
+    await zip.extract(download_path, cadSdkDirectory);
+    // 自动生成vscode配置文件，以及cmake文件
 };
 
 await main();
