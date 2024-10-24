4877638f2338bc6ea35b4c9c1b75b74990c59813
Cangjier <moodlee@qq.com>
2024-10-24 23:45:04 +0800

    optimize cmake

--- cli/cmake/main.ts
+++ cli/cmake/main.ts
@@ -34,7 +34,7 @@ let main = async () => {
     else{
         cmakeLines.push(`set(${projectName}_INCLUDE_DIR "\${${projectName}_DIR}")`);
     }
-    await File.WriteAllTextAsync(Path.Combine(projectDirectory,`${projectName}.cmake`),cmakeLines.join("\n"),utf8);
+    await File.WriteAllTextAsync(Path.Combine(projectDirectory,`Find${projectName}.cmake`),cmakeLines.join("\n"),utf8);
     
 };
 
