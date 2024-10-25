6ba996af371636d166565997ea42fe6c71437cfe
Cangjier <moodlee@qq.com>
2024-10-25 23:46:46 +0800

    init

--- cli/nx/main.ts
+++ cli/nx/main.ts
@@ -75,21 +75,21 @@ let cmd_init = async () => {
     if (Directory.Exists(vscodeDirectory) == false) {
         Directory.CreateDirectory(vscodeDirectory);
     }
-    let vscodeSettingsPath = Path.Combine(vscodeDirectory, "settings.json");
-    let vscodeSettingsText = await File.ReadAllTextAsync(Path.Combine(script_directory, ".vscode", "settings.json"), utf8);
-    await File.WriteAllTextAsync(vscodeSettingsPath, vscodeSettingsText, utf8);
+    // let vscodeSettingsPath = Path.Combine(vscodeDirectory, "settings.json");
+    // let vscodeSettingsText = await File.ReadAllTextAsync(Path.Combine(script_directory, ".vscode", "settings.json"), utf8);
+    // await File.WriteAllTextAsync(vscodeSettingsPath, vscodeSettingsText, utf8);
     // 自动创建.vscode/c_cpp_properties.json
-    let vscodeCppPropertiesPath = Path.Combine(vscodeDirectory, "c_cpp_properties.json");
-    let vscodeCppPropertiesText = await File.ReadAllTextAsync(Path.Combine(script_directory, ".vscode", "c_cpp_properties.json"), utf8);
-    await File.WriteAllTextAsync(vscodeCppPropertiesPath, vscodeCppPropertiesText, utf8);
+    // let vscodeCppPropertiesPath = Path.Combine(vscodeDirectory, "c_cpp_properties.json");
+    // let vscodeCppPropertiesText = await File.ReadAllTextAsync(Path.Combine(script_directory, ".vscode", "c_cpp_properties.json"), utf8);
+    // await File.WriteAllTextAsync(vscodeCppPropertiesPath, vscodeCppPropertiesText, utf8);
     // 自动创建.vscode/tasks.json
-    let vscodeTasksPath = Path.Combine(vscodeDirectory, "tasks.json");
-    let vscodeTasksText = await File.ReadAllTextAsync(Path.Combine(script_directory, ".vscode", "tasks.json"), utf8);
-    await File.WriteAllTextAsync(vscodeTasksPath, vscodeTasksText, utf8);
+    // let vscodeTasksPath = Path.Combine(vscodeDirectory, "tasks.json");
+    // let vscodeTasksText = await File.ReadAllTextAsync(Path.Combine(script_directory, ".vscode", "tasks.json"), utf8);
+    // await File.WriteAllTextAsync(vscodeTasksPath, vscodeTasksText, utf8);
     // 自动创建.vscode/launch.json
-    let vscodeLaunchPath = Path.Combine(vscodeDirectory, "launch.json");
-    let vscodeLaunchText = await File.ReadAllTextAsync(Path.Combine(script_directory, ".vscode", "launch.json"), utf8);
-    await File.WriteAllTextAsync(vscodeLaunchPath, vscodeLaunchText, utf8);
+    // let vscodeLaunchPath = Path.Combine(vscodeDirectory, "launch.json");
+    // let vscodeLaunchText = await File.ReadAllTextAsync(Path.Combine(script_directory, ".vscode", "launch.json"), utf8);
+    // await File.WriteAllTextAsync(vscodeLaunchPath, vscodeLaunchText, utf8);
     // 自动创建 main.cpp
     let mainCppPath = Path.Combine(projectDirectory, "main.cpp");
     let templateMainCppPath = Path.Combine(script_directory, "template.cpp");
