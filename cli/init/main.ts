6d019717ba0ea24a9902ebd9db69fcc28ce5fbd9
Cangjier <moodlee@qq.com>
2024-11-07 18:26:13 +0800

    optimize

--- cli/init/main.ts
+++ cli/init/main.ts
@@ -115,7 +115,7 @@ let gitManager = GitManager();
 
 let CMakeManager = () => {
     let checkInstalled = async () => {
-        let output = {lines:[]} as { lines: string[] };
+        let output = { lines: [] } as { lines: string[] };
         await cmdAsync(Environment.CurrentDirectory, "cmake --version", output);
         if (output.lines && output.lines.length > 0) {
             return true;
@@ -158,7 +158,7 @@ let cmakeManager = CMakeManager();
 
 let VsCodeManager = () => {
     let checkInstalled = async () => {
-        let output = {lines:[]} as { lines: string[] };
+        let output = { lines: [] } as { lines: string[] };
         await cmdAsync(Environment.CurrentDirectory, "code --version", output);
         if (output.lines && output.lines.length > 0) {
             return true;
@@ -245,7 +245,7 @@ let visualStudioManager = VisualStudioManager();
 
 let VcpkgManager = () => {
     let checkInstalled = async () => {
-        let output = {lines:[]} as { lines: string[] };
+        let output = { lines: [] } as { lines: string[] };
         await cmdAsync(Environment.CurrentDirectory, "vcpkg version", output);
         if (output.lines && output.lines.length > 0) {
             return true;
@@ -342,7 +342,7 @@ let SDKManager = () => {
 let sdkManager = SDKManager();
 
 let main = async () => {
-    
+
 
     if (await gitManager.clone() == false) {
         return;
