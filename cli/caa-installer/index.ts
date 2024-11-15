62ec2b0b4fe505b69a4820942b36c858ae80ab88
Cangjier <moodlee@qq.com>
2024-11-15 15:24:38 +0800

    optimize caa-installer

--- cli/caa-installer/index.ts
+++ cli/caa-installer/index.ts
@@ -651,12 +651,12 @@ let InstallerR21 = () => {
         let dslsPath = Path.Combine(archiveDirectory, "6", "_SolidSQUAD_", "DSLS_SSQ_V6R2017x_Installer_20170620.exe");
         let catiaSSQ = "CATIA V5R21-V5R22-V23.SSQ";
         let caaSSQ = "CAA Rade V5R21-V5R22.SSQ";
-        // await installCatia(catiaDirectory);
-        // await installCatiaCrack(crackArchivePath);
-        // await installCAA(caaArchivePath);
-        // await installRade(radeArchivePath);
-        // await installDotNet(dotnet35Path);
-        // await installDotNet(dotnet20Path);
+        await installCatia(catiaDirectory);
+        await installCatiaCrack(crackArchivePath);
+        await installCAA(caaArchivePath);
+        await installRade(radeArchivePath);
+        await installDotNet(dotnet35Path);
+        await installDotNet(dotnet20Path);
         await installVS2008(vs2008Path);
         await installVS2008SP1(vs2008SP1Path);
         await installDSLS(dslsPath);
