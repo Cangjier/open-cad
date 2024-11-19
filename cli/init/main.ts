065af989f48f9ef0fa3693555429d74c36807b86
Cangjier <moodlee@qq.com>
2024-11-19 19:43:33 +0800

    optimize caa-installer

--- cli/init/main.ts
+++ cli/init/main.ts
@@ -370,7 +370,7 @@ let main = async () => {
     // 检查visual studio环境变量
     await visualStudioManager.resgiterEnvironment();
 
-    await cmdAsync(Environment.CurrentDirectory, `opencad ${cadName} init ${cadSDK.name}`);
+    await cmdAsync(Environment.CurrentDirectory, `opencad ${cadName}-init init ${cadSDK.name}`);
 
 };
 
