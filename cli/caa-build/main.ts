81f0a1bed9e6ffb647e2c768090ccae86733674d
Cangjier <moodlee@qq.com>
2024-11-19 21:17:53 +0800

    optimize caa-installer

--- cli/caa-build/main.ts
+++ cli/caa-build/main.ts
@@ -7,10 +7,13 @@ let Rade = () => {
     let tck_initPath = "C:\\Program Files (x86)\\Dassault Systemes\\B21\\intel_a\\code\\command\\tck_init.bat";
     let build = async (frameworkDirectory: string) => {
         let sh = shell.start({
-            filePath: tck_initPath,
+            filePath: "cmd",
             workingDirectory: frameworkDirectory
         });
         await Task.Delay(1000);
+        sh.writeLine(`call "${tck_initPath}"`);
+        await Task.Delay(1000);
+        console.log(sh.readLines());
         sh.writeLine("tck_list");
         await Task.Delay(1000);
         console.log(sh.readLines());
