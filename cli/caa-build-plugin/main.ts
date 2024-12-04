bdfa549d15251236139e3afb4a9e1fc864acc5a5
Cangjier <moodlee@qq.com>
2024-12-04 06:34:48 +0000

    optimize

--- cli/caa-build-plugin/main.ts
+++ cli/caa-build-plugin/main.ts
@@ -256,7 +256,7 @@ let main = async () => {
                 }
                 let errorMessage = toReportLines.join("\n");
                 let isSuccess = toReportLines.length == 0;
-                let headerMessage = `${isSuccess ? "✅" : "❌"} ${Path.GetFileName(gitUrl)} ${tagName} ${isSuccess ? "编译成功" : "编译失败"} ${message ?? ""}`;
+                let headerMessage = `${isSuccess ? "✅" : "❌"} ${Path.GetFileName(gitUrl)} ${tagName} ${isSuccess ? "Build Succeed" : "Build Failed"} ${message ?? ""}`;
                 let finalMessage = isSuccess ? `${headerMessage}\r\n${testcaseMessageLines.join('\r\n')}` : `${headerMessage}\r\n${errorMessage}`;
 
                 await axios.post(`${stringUtils.trimEnd(server, "/")}/api/v1/tasks/run`, {
